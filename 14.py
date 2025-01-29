14d614e15bbd1b0ac8e799b2ba49ec54973bbb47
BharathShankarReddy <bharathpothireddy406@gmail.com>
2025-01-29 11:07:20 +0530

    Create 14.py

---
+++ 14.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+np.random.seed(42)
+data = np.random.randint(20,81,1000)
+plt.hist(data, bins=15, edgecolor='black',color='green')
+plt.title('Histogram of Cancer Patients Age Distribution')
+plt.xlabel('Age')
+plt.ylabel('Numnber of Patients')
+plt.show()
