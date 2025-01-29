1d0b1e01398567d657bc077199b76e43e6d8466d
BharathShankarReddy <bharathpothireddy406@gmail.com>
2025-01-29 11:01:22 +0530

    7.py

---
+++ 7.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+#creating a sample dataframe
+data = pd.DataFrame({'Brand':['Maruti','Hyundai','Tata','Mahindra','Maruti','Renault','Tata','Maruti'],
+                     'Year':[2012,2014,2011,2015,2012,2016,2014,2018]
+                     })
